2b1dbb7ec11fe8f689fdf81b6d89011be82dbf2b
Andrey Korovin <misticwonder@gmail.com>
2026-01-04 16:33:35 +0200

    Update FastifyTransport with a basic route and rename instance variable

--- src/transports/fastify/fastify.transport.ts
+++ src/transports/fastify/fastify.transport.ts
@@ -1,15 +1,19 @@
-import Fastify, { type FastifyInstance } from 'fastify';
+import Fastify, { type FastifyInstance, type FastifyReply, type FastifyRequest } from 'fastify';
 import type { Transport } from '../../libs/kernel/index.js';
 
 export class FastifyTransport implements Transport {
   start(): Promise<void> | void {
-    const fastify: FastifyInstance = Fastify({
+    const server: FastifyInstance = Fastify({
       logger: true,
     });
 
-    fastify.listen({ port: 3000 }, function (err) {
+    server.get('/', async function (_request: FastifyRequest, _reply: FastifyReply): Promise<any> {
+      return { hello: 'world' };
+    });
+
+    server.listen({ port: 3000 }, function (err) {
       if (err) {
-        fastify.log.error(err);
+        server.log.error(err);
         process.exit(1);
       }
     });
