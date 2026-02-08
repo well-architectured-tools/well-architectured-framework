42f0990fe81b2430a7b97f6a3d5919eae33c2c51
Andrey Korovin <misticwonder@gmail.com>
2026-02-08 11:40:58 +0200

    Add `pino-pretty` dependency to package.json and update package-lock.json

--- src/libs/logger/pino-logger-service.ts
+++ src/libs/logger/pino-logger-service.ts
@@ -1,13 +1,15 @@
 import pino from 'pino';
 import type { LoggerService } from './logger-service.js';
 import { ApplicationError, errorToStringWithCauses } from '../errors/index.js';
-import type { EnvironmentService } from '../environment/index.js';
+import type { EnvironmentService, EnvironmentVariables } from '../environment/index.js';
 
 export class PinoLoggerService implements LoggerService {
+  private readonly environmentService: EnvironmentService;
   private logger: pino.Logger;
 
   constructor(environmentService: EnvironmentService) {
-    this.logger = pino({ level: environmentService.get('LOG_LEVEL') });
+    this.environmentService = environmentService;
+    this.logger = pino(this.getLoggerOptions());
   }
 
   info(message: string, data?: Record<string, unknown>): void {
@@ -58,5 +60,27 @@ export class PinoLoggerService implements LoggerService {
     } else {
       this.logger.error(data, message);
     }
+  }
+
+  private getLoggerOptions(): pino.LoggerOptions {
+    const nodeEnv: EnvironmentVariables['NODE_ENV'] = this.environmentService.get('NODE_ENV');
+    const logLevel: EnvironmentVariables['LOG_LEVEL'] = this.environmentService.get('LOG_LEVEL');
+
+    const optionsMap: Record<EnvironmentVariables['NODE_ENV'], pino.LoggerOptions> = {
+      development: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'SYS:isoTime',
+            ignore: 'pid,hostname',
+          },
+        },
+        level: logLevel,
+      },
+      production: { level: logLevel },
+    };
+
+    return optionsMap[nodeEnv];
   }
 }
