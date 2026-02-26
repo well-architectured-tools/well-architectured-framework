ee1055186d3f6b1b1851fd81b0f3f7c4829a9e6e
Andrey Korovin <misticwonder@gmail.com>
2026-02-26 11:24:17 +0200

    Set NODE_ENV to test in vitest.global.ts for test environment configuration

--- vitest.global.ts
+++ vitest.global.ts
@@ -1,7 +1,8 @@
 import { config } from 'dotenv-safe';
 
 export default function setup(): void {
-  process.env['DOTENV_SAFE_LOADED'] = 'true';
+  process.env['NODE_ENV'] = 'test';
   process.env['LOG_LEVEL'] = 'warn';
+  process.env['DOTENV_SAFE_LOADED'] = 'true';
   config();
 }
