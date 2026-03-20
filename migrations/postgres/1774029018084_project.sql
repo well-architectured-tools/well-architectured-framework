276e9430aae5063143c019da86383b319f35138c
Andrey Korovin <misticwonder@gmail.com>
2026-03-20 19:54:32 +0200

    Add SQL script to create project table with primary key

---
+++ migrations/postgres/1774029018084_project.sql
@@ -0,0 +1,7 @@
+CREATE TABLE waf.project
+(
+  id         UUID        NOT NULL,
+  name       TEXT        NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL,
+  CONSTRAINT project_pkey PRIMARY KEY (id)
+);
---
+++ src/modules/projects/domain/aggregates/project.aggregate.ts
@@ -0,0 +1,28 @@
+import { AggregateRoot, DateTime, UuidV7 } from '../../../../libs/ddd/index.js';
+
+interface Props {
+  name: string;
+  createdAt: DateTime;
+}
+
+export class Project extends AggregateRoot<Props> {
+  get name(): string {
+    return this.props.name;
+  }
+
+  get createdAt(): DateTime {
+    return this.props.createdAt;
+  }
+
+  static create(props: Omit<Props, 'createdAt'>): Project {
+    const createdAt: DateTime = DateTime.createNow();
+    return new this({
+      ...props,
+      createdAt,
+    });
+  }
+
+  static rehydrate(props: Props, id: UuidV7): Project {
+    return new this(props, id);
+  }
+}
