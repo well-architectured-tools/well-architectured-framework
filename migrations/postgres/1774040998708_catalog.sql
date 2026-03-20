51f155c73dcf0f76d46d39a8ac77af7a3e947d12
Andrey Korovin <misticwonder@gmail.com>
2026-03-20 23:12:56 +0200

    Add SQL script to create catalog table with primary key and foreign key constraint

---
+++ migrations/postgres/1774040998708_catalog.sql
@@ -0,0 +1,9 @@
+CREATE TABLE waf.catalog
+(
+  id         UUID        NOT NULL,
+  name       TEXT        NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL,
+  project_id UUID        NOT NULL,
+  CONSTRAINT catalog_pkey PRIMARY KEY (id),
+  CONSTRAINT catalog_project_id_fkey FOREIGN KEY (project_id) REFERENCES waf.project (id) ON DELETE CASCADE
+);
